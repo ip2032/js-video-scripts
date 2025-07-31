9ba0e7bbc15ccdd87d1abc8a93965fa5efab561e
ip2032 <142797111+ip2032@users.noreply.github.com>
2025-07-31 12:14:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-
+var CONFIG = {
+    OMNIDESK_API_KEY: 'YOUR_API_KEY_HERE',
+    OMNIDESK_DOMAIN: 'yourdomain.omnidesk.ru',
+    API_BASE_URL: 'https://yourdomain.omnidesk.ru/api/v1',
+    TEMPLATES: {
+        ACTION: 000000,
+        REALIZATION: 000000,
+        CATALOG: 000000
+    }
+};
